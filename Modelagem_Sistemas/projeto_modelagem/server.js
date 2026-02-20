7fba9817a059f3d2f52c1564b33914182c862f13
naydourado <naydouradooliveira@gmail.com>
2026-02-20 07:38:32 -0300

    first class

---
+++ Modelagem_Sistemas/projeto_modelagem/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+const app = express()
+app.use(express.json()) // especificando ao JS para usar JSON no express
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+    users.push(req.body)
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', (req, res) => {
+    res.status(200).json(users)
+})
+
+app.listen(3000)
