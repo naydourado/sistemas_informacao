954e6c82d1fdd356349466ef38a6f0368992aedd
EA - Nayra Dourado Oliveira <nayra_oliveira@acad.unasp.edu.br>
2026-02-26 22:45:27 -0300

    up

---
+++ Modelagem_Sistemas/sistema_login/login.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql");
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+
+// MySQL - Na opinião do professor, o melhor para aplicações Web
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodelogin'
+});
+
+const app = express();
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', function(request, response){
+    response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.listen(1950);
